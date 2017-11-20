1901b6e45c0812f6329fdeab60508e1a584335ff
Denise Buder <denisebuder@c78-104-169-108.fh-salzburg.ac.at>
2017-11-20 16:08:39 +0100

    responsive added

---
+++ src/nav.js
@@ -0,0 +1,5 @@
+function dropDown() {
+    document.getElementById("navDrop").classList.toggle("drop");
+    document.getElementById("burgerDrop").classList.toggle("x");
+    document.getElementById("header").classList.toggle("h");
+}
